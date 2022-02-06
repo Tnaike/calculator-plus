d5db5679542b020bd32c091c0fb13c42e2c154c1
Tnaike <timmynaike@gmail.com>
2022-02-06 09:02:25 +0100

    adjust input field style

--- src/components/CurrencyConverter/CurrencyConverter.js
+++ src/components/CurrencyConverter/CurrencyConverter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import './currencyStyle.css';
 
 const DisplayResult = styled.div`
   display: block;
@@ -16,15 +17,45 @@ const DisplayResult = styled.div`
 const CurrencyConverter = () => {
   return (
     <>
-      <div className='wrap-input'>
-        <input
-          type='number'
-          className='input-field'
-          placeholder=''
-          min='1'
-          required
-        />
+    <div className='wrap-input'>
+      <div className='input-group'>
+        <select className='form-select' id='inputGroupSelect01'>
+          <option value='NGN' selected>
+            ₦ - NGN
+          </option>
+          <option value='USD'>$ - USD</option>
+          <option value='Euro'>€ - Euro</option>
+          <option value='GBP'>£ - GBP</option>
+          <option value='AED'>د.إ - AED</option>
+          <option value='Yuan'>¥ - Yuan</option>
+          <option value='ZAR'>R - ZAR</option>
+          <option value='KES'>Ksh - KES</option>
+          <option value='CHF'>Fr - CHF</option>
+          <option value='INR'>₹ - INR</option>
+        </select>
+        <input type='number' className='input-control' min='0' required/>
       </div>
+    </div>
+      <div className='wrap-input'>
+        <div className='input-group'>
+          <select className='form-select' id='inputGroupSelect01'>
+            <option value='NGN'>
+              ₦ - NGN
+            </option>
+            <option value='USD' selected>$ - USD</option>
+            <option value='Euro'>€ - Euro</option>
+            <option value='GBP'>£ - GBP</option>
+            <option value='AED'>د.إ - AED</option>
+            <option value='Yuan'>¥ - Yuan</option>
+            <option value='ZAR'>R - ZAR</option>
+            <option value='KES'>Ksh - KES</option>
+            <option value='CHF'>Fr - CHF</option>
+            <option value='INR'>₹ - INR</option>
+          </select>
+          <input type='number' className='input-control' min='0' required/>
+        </div>
+      </div>
+
       <div className='wrap-input'>
         <input
           type='number'
@@ -39,7 +70,7 @@ const CurrencyConverter = () => {
       </div>
       <DisplayResult>
         <p className='resultFigure'>$2 == ₦830</p>
-        <p className='resultRate mt-5'>Rate:{' '} ₦415</p>
+        <p className='resultRate mt-5'>Rate: ₦415</p>
       </DisplayResult>
     </>
   );
